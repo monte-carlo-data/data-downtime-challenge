7774097ce4a1290e300ce7acce16ac6d1f55261e
Ryan Othniel Kearns <ryankearns25@gmail.com>
2020-11-26 17:12:34 +0000

    Experiment with some sample data

---
+++ helpers/make_data.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = "Ryan Othniel Kearns"
+__maintainer__ = "Ryan Othniel Kearns"
+__email__ = "rkearns@montecarlodata.com"
+
+"""TODO DESCRIBE
+"""
+
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta
+
+NUM_SAMPLES = 400
+
+dist = np.random.normal(loc=50000, scale=10000, size=NUM_SAMPLES)
+start_date = datetime(2020, 11, 1)
+timestamps = np.array([start_date + timedelta(hours=i) for i in range(NUM_SAMPLES)])
+df = pd.DataFrame(columns=["table_id", "metric", "field"])
+df["timestamp"] = timestamps
+df["value"] = dist
+df["table_id"] = "test_table_0"
+df["metric"] = "row_count"
+df["field"] = None
+
+# introduce anomalies
+df.loc[107:109, ["value"]] /= 2
+df.loc[110, ["value"]] = 157
+df.loc[111:115, ["value"]] = 0
+
+df.to_csv("data/ex_1.csv")
